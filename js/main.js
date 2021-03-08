5eab68878da10e30e6950699ffe8565a44bb7294
ana467 <anastassiajurina05@gmail.com>
2021-03-08 20:30:05 +0200

    javascript

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//alert('Hello World')
+let undf
+let nll = null
+let arr = []
+let obj = {}
+
+
+Number()
+//parseInt()
+//parseFloat()
+
+let cars2 = {
+    key:'string',
+    key2:100,
+    key3:function(){
+        return ''
+    },
+    key4:() => {
+
+    }
+}
+
+function name(){}
