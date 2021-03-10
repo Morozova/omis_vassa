fa168cae6d040ec5c68799bac1ca734dc3db6383
Morozova <vmorozova08@gmail.com>
2021-03-10 20:38:43 +0200

    msg

--- js/newsapi.js
+++ js/newsapi.js
@@ -1,22 +1,39 @@
 
 class Newsapi{
-    url = "http://newsapi.org/v2/everything?q=tesla&from=2021-02-08&sortBy=publishedAt&apiKey=5276d87af504489e9339985ab691991a"
+    apiKey = "5276d87af504489e9339985ab691991a"
+    urlTemplate = "http://newsapi.org/v2/everything?"
+    attributes = {
+        q:'tesla',
+        from:'2021-02-10',
+        sortBy:'publishedAt',
+        apiKey:this.apiKey
+    }
+    //q=tesla&from=2021-02-10&sortBy=publishedAt&apiKey=5276d87af504489e9339985ab691991a"
 
     getData(){
+        console.log('getData')
+
+        this.generateURL()
 
         fetch(this.url)
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => {
+                console.log(data)
+                this.parseNewsObject(data.articles)
+
+                let webpage = document.getElementById('news')
+                webpage.innerHTML = news.getAllCardsHTML()
+            });
     }
 
     generateNewsCard(){
         let card = `
         <div class="card" style="width: 18rem;">
-        ${this.url}
-        <img src="..." class="card-img-top" alt="...">
+        ${this.getNewsImage()}
+        
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 class="card-title">${this.getNewsTitle()}</h5>
+          <p class="card-text">${this.getNewsBody()}</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
@@ -25,30 +42,74 @@ class Newsapi{
         return card
     }
 
-    getNewsImage(){}
-    getNewsTitle(){}
-    getNewsBody(){}
+    getNewsTitle(){
+        return this._NewsTitle
+    }
+    getNewsBody(){
+        return this._NewsBody
+    }
+    getNewsImage(){
+        let img = `<img src="${this._NewsImage}" class="card-img-top" alt="${this.getNewsTitle()}">`
+        return img
+    }
+
+    getAllCardsHTML(){
+        console.log('getAllCardsHTML')
+        return this._cards
+    }
 
     parseNewsObject(newsAllData){
 
         if (typeof newsAllData == "object"){
-        newsAllData.forEach(element => {
-            this.newsItem(element)
+            this._cards = ''
+            newsAllData.forEach(element => {
+            this.parseNewsItem(element)
+            this._cards += this.generateNewsCard()
         });
     }
     }
 
-    newsItem(newsItem){
+    parseNewsItem(newsItem){
         if(typeof newsItem == "object")
         {
             this._NewsTitle = newsItem.title
             this._NewsBody = newsItem.content
-            this._NewImage = newsItem.urlToImage
+            this._NewsImage = newsItem.urlToImage
         }
     }
 
+    generateURL(){
+        let attributes = ''
+        Object.entries(this.attributes).forEach(element => {
+            console.log(element)
+            attributes += element[0] + '=' + element[1] + '&'
+        })
+        this.url = this.urlTemplate + attributes.substr(0, this.attributes.length - 1)
+        //v1
+        //v2
+        //Object.entries(this.attributes).join('&)
 
+    }
 }
 
 let news = new Newsapi()
 news.getData()
+
+//search
+let frm = document.querySelector('form')
+let btn = frm.querySelector('button[type=button]')
+let q = frm.querySelector('input[name=queryKeyword]')
+
+btn.addEventListener('click', function(event){
+    console.log('you clicked the button')
+    console.log(event)
+    //get input value and set it in class
+    
+    news.attributes.q = q.value
+    news.getData()
+    //if (event.ctrlKey == true) console.log(q.value)
+})
+btn.addEventListener('dblclick', function(){
+    console.log('you double clicked the button')
+})
+
