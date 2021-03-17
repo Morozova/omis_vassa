1253ddd51912d41262eff1af427d81b0a833632b
Morozova <vmorozova08@gmail.com>
2021-03-17 20:39:29 +0200

    front + back

---
+++ bookshelf/backend/IBook.ts
@@ -0,0 +1,9 @@
+//интерфейс - перечисление переменных и функций без отпределения тела
+
+export interface IBook {
+    isbn:string
+    title:string
+    author:string
+    description:string
+    publish_date:string
+}
---
+++ bookshelf/backend/index.ts
@@ -0,0 +1,55 @@
+import { IBook } from './IBook'
+
+const express = require('express')
+const app = express()
+
+let port = 3000
+
+let books:IBook[] = [
+    {
+    isbn:'isbn1',
+    title:'title 1',
+    author:'author 1',
+    description:'description 1',
+    publish_date:'publish_date 1'
+},
+{
+    isbn:'isbn2',
+    title:'title 2',
+    author:'author 2',
+    description:'description 2',
+    publish_date:'publish_date 2'
+},
+{
+    isbn:'isbn3',
+    title:'title 3',
+    author:'author 3',
+    description:'description 3',
+    publish_date:'publish_date 3'
+}
+]
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/books', function (req, res) {
+    res.send(books)
+})
+
+app.delete('/books/delete/:isbn', function (req, res) {
+    console.log(req.params.isbn)
+
+    let msg = 'Book is not found'
+
+    books.forEach( (book, index) => {
+        if (book.isbn == req.params.isbn){
+            books.splice(index, 1)
+            msg = 'Book successfully deleted'
+        }
+    })
+
+    res.send(msg)
+})
+
+app.listen(port, () => console.log('server is working'))
